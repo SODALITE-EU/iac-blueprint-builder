788f20a07eeae4dd55c7ff9f1a6fe392794b4b75
Askarpour <mehrnoosh.askarpour@gmail.com>
2020-01-10 16:51:59 +0100

    updates

--- parser.py
+++ parser.py
@@ -1,27 +1,32 @@
 import json
 
-def Convert(lst):
-    res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
-    return res_dct
-
 def innerdicts(data,tabs, outfile):
     for key, value in data.items():
-        if tabs == 0: outfile.write('\n')
         if isinstance(value,list):
-            value = value[0]
+            if isinstance(value[0],dict):
+                v = {}
+                for i in value:
+                    v.update(i)
+                value = v
+            # else:
+            #     value = [a[a.rfind('/')+1:] for a in value if "https://" in a]
         if isinstance(value,dict):
+            if "https://" in str(key):
+                key = str(key)[str(key).rfind('/')+1:]
             outfile.write('    '*(tabs)  + str(key) + ':  \n')
             innerdicts(value,tabs+1,outfile)
         else:
             outfile.write('    '*(tabs) + str(key) + ': ' + str(value) + ' \n')
 
-
 def parse_file(filename):
     with open(filename) as file:
         data = json.load(file)
     outfile = open(filename.replace(".json",".yml"), "w")
-    innerdicts(data,0, outfile)
-
+    outfile.write('tosca_definitions_version: tosca_simple_yaml_1_0 \ndescription: Template for deploying a single %s' %list(data)[0])
+    innerdicts(data[list(data)[0]],1, outfile)
+    outfile.write('\ntopology_template: \n')
+    del data[list(data)[0]]
+    innerdicts(data,1, outfile)
 
 
 data = parse_file("snowUC-testbed.json")
