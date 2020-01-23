2c7fe2068f385745065a0ecc0ff17537f695724e
Askarpour <mehrnoosh.askarpour@gmail.com>
2020-01-23 10:26:04 +0100

    creating separate folders for concurrent requests + adding teh current blup2json script for the purpose of testing

--- main.py
+++ main.py
@@ -3,27 +3,31 @@ import requests, os
 import blueprint2json
 from gevent.pywsgi import WSGIServer
 import parser
+import uuid
 
 app = Flask(__name__)
 
 @app.route('/parse', methods = ['POST'])
 def parse():
     body = request.get_json()
-    ansibles = parser.parse_data(body["name"], body["data"])
-
+    workpath = '%s-%d' % (body["name"], uuid.uuid1())
+    if not os.path.exists(workpath):
+        os.makedirs(workpath)
+    outpath = os.path.join(workpath, body["name"])
+    ansibles = parser.parse_data(outpath, body["data"])
     print('Reading Ansible files ---------')
     for url in ansibles:
         print('Reading   %s ------- '%url)
         temp = str(url).split('/')
         filename = temp[-1]
-        foldername = os.path.join(*temp[4:-1])
+        foldername = os.path.join(workpath, *temp[4:-1])
         if not os.path.exists(foldername):
             os.makedirs(foldername)
         outfile = open(os.path.join(foldername, filename), "w")
         outfile.write(str(requests.get(url).content))
         print('Ansible files done ------- ')
-        print('blueprint2json ongoing ------- ')
-        os.system('python3 blueprint2json.py a test2.yml > Tosca_ansible.json')
+    print('blueprint2json ongoing ------- ')
+    os.system('python3 blueprint2json.py %s %s.yml > %s.json' % (body["name"], outpath, outpath))
     return "OK"
 
 if __name__ == '__main__':
