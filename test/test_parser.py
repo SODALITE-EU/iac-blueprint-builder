53bf8cbb098c58cd3c1487eb582fdcd471022e4c
Askarpour <mehrnoosh.askarpour@gmail.com>
2020-04-01 13:55:55 -0400

    try a new test for Jenkins

--- test/test_parser.py
+++ test/test_parser.py
@@ -8,17 +8,15 @@ from src import parse
 def test_parser():
     expected = [
         'http://160.40.52.200:8084/Ansibles/playbooks/docker/dockerized_component_undeploy.yml',
-        'http://160.40.52.200:8084/Ansibles/playbooks/docker/dockerized_component_deploy.yml', 
-        'http://160.40.52.200:8084/Ansibles/playbooks/openstack/vm/delete.yml', 
-        'http://160.40.52.200:8084/Ansibles/playbooks/openstack/vm/create.yml', 
-        'http://160.40.52.200:8084/Ansibles/playbooks/openstack/security-rule/create.yml', 
-        'http://160.40.52.200:8084/Ansibles/playbooks/openstack/security-rule/delete.yml', 
-        'http://160.40.52.200:8084/Ansibles/playbooks/docker/destroy_docker_host.yml', 
+        'http://160.40.52.200:8084/Ansibles/playbooks/docker/dockerized_component_deploy.yml',
+        'http://160.40.52.200:8084/Ansibles/playbooks/openstack/vm/delete.yml',
+        'http://160.40.52.200:8084/Ansibles/playbooks/openstack/vm/create.yml',
+        'http://160.40.52.200:8084/Ansibles/playbooks/openstack/security-rule/create.yml',
+        'http://160.40.52.200:8084/Ansibles/playbooks/openstack/security-rule/delete.yml',
+        'http://160.40.52.200:8084/Ansibles/playbooks/docker/destroy_docker_host.yml',
         'http://160.40.52.200:8084/Ansibles/playbooks/docker/create_docker_host.yml']
-    
+
     with open('test/fixture.json') as f:
         t = json.load(f)
-        assert parse(t["data"]) == expected
-
-
-
+        # assert parse(t["data"]) == expected
+        assert set(parse(t["data"])) == set(expected)
