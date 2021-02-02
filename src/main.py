aacd0f03171bb51cfab73e707035249a08228a33
Saloni Kyal <salonikyal1996@gmail.com>
2021-02-02 09:37:48 +0000

    Hotspots reviewed. Refactored main.py.

--- src/main.py
+++ src/main.py
@@ -1,46 +1,66 @@
 import json
 import os
 import uuid
-
+import pathlib
 import requests
+
 from flask import Flask, request
 from flask_wtf.csrf import CSRFProtect
 from flask_swagger_ui import get_swaggerui_blueprint
 
 import src
 
-SWAGGER_URL = '/docs'
-API_URL = '/static/swagger.json'
-XOPERA_ENDPOINT_KEY = 'XOPERA_ENDPOINT'
-XOPERA_ENDPOINT_DEFAULT = 'https://154.48.185.209'
-CONFIG_PATH = 'config.json'
+class XoperaConfig:
+
+    config_path = pathlib.Path(__file__).parent / pathlib.Path('../config_xopera.json')
+    config = None
+
+    @classmethod
+    def init(cls):
+        if not cls.config:
+            cls.config = json.load(cls.config_path.open())
+
+    @classmethod
+    def get_xopera_endpoint(cls):
+        cls.init()
+        return os.getenv("XOPERA_ENDPOINT", cls.config.get("XOPERA_ENDPOINT", "localhost"))
+
+    @classmethod
+    def get_xopera_swagger_url(cls):
+        cls.init()
+        swagger_url = os.getenv("SWAGGER_URL", cls.config.get("SWAGGER_URL", "/docs"))
+        return swagger_url
+
+    @classmethod
+    def get_xopera_api_url(cls):
+        cls.init()
+        api_url = os.getenv("API_URL", cls.config.get("API_URL", "/static/swagger.json"))
+        return api_url
+
+    @classmethod
+    def get_app_name(cls):
+        cls.init()
+        app_name = os.getenv("app_name", cls.config.get("app_name", "SODALITE iac-blueprint-builder"))
+        return app_name
+
+    @classmethod
+    def get_xopera_api(cls):
+        cls.init()
+        manage_url = os.getenv("MANAGE_URL", cls.config.get("MANAGE_URL", "/manage"))
+        return cls.get_xopera_endpoint() + manage_url
+
 csrf = CSRFProtect()
 app = Flask(__name__)
 csrf.init_app(app)
 
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "SODALITE iac-blueprint-builder"
-    }
+    XoperaConfig.get_xopera_swagger_url(),
+    XoperaConfig.get_xopera_api_url(),
+    XoperaConfig.get_app_name(),
 )
-app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=XoperaConfig.get_xopera_swagger_url())
 
-XOPERA_ENDPOINT = os.getenv(XOPERA_ENDPOINT_KEY)
-
-if not XOPERA_ENDPOINT:
-    if os.path.exists(CONFIG_PATH):
-        with open(CONFIG_PATH) as config:
-            config = json.load(config)
-            XOPERA_ENDPOINT = config[XOPERA_ENDPOINT_KEY]
-    else:
-        XOPERA_ENDPOINT = XOPERA_ENDPOINT_DEFAULT
-
-XOPERA_API = os.path.join(XOPERA_ENDPOINT, "manage")
-
-print("starting with XOPERA endpoint:", XOPERA_API)
-
+print("Starting with XOPERA endpoint:", XoperaConfig.get_xopera_api())
 
 @app.route('/parse', methods=['POST'])
 def parse():
@@ -60,9 +80,8 @@ def parse():
     os.system('python3 src/blueprint2CSAR.py %s %s --entry-definitions %s.yml --output %s' %
               (body["name"], outpath[:outpath.rfind('/')], body["name"], outpath))
     files = [('CSAR', open('%s.zip' % (outpath,), 'rb'))]
-    response = requests.post(XOPERA_API, files=files, verify=True)
+    response = requests.post(XoperaConfig.get_xopera_api(), files=files, verify=True)
     return json.loads(response.text)
-
 
 def download_dependencies(urls, filenames, workpath):
     for url, filename in zip(urls, filenames):
@@ -74,7 +93,6 @@ def download_dependencies(urls, filenames, workpath):
         outfile = open(os.path.join(workpath, filename), "w")
         outfile.write(str(requests.get(url).text))
         outfile.close()
-
 
 '''
 if __name__ == '__main__':
